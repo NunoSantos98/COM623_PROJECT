0f1a5654894648925320c208796fa598547806c7
Nuno Santos <55833020+NunoSantos98@users.noreply.github.com>
2021-12-29 22:36:20 +0000

    first commit

---
+++ build/precache-manifest.53fbc095c79f0bc7e4b6a70dcddb1432.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9bc777f79729bfd7fbf994774674fb9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ba3df12a89a729424062",
+    "url": "./static/js/2.15b392a3.chunk.js"
+  },
+  {
+    "revision": "2e629291cd4c2a7a167e",
+    "url": "./static/js/main.5c9c5de3.chunk.js"
+  },
+  {
+    "revision": "ef256377240f6dafb3cd",
+    "url": "./static/js/runtime~main.dea7d9b8.js"
+  },
+  {
+    "revision": "6e8575eb8b40935f111067be6ad23d89",
+    "url": "./static/media/backgroundimg.6e8575eb.png"
+  }
+]);
