cdb0c39ca7e90930b00b46bda43e85b66a96029e
NunoSantos98 <4goncn14@solent.ac.uk>
2022-01-14 06:08:56 +0000

    Updates

---
+++ precache-manifest.70282779c9f0f350520f84d7c88ff50c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c13e1847d573093d9d1ce5837e12554",
+    "url": "/COM623_PROJECT/index.html"
+  },
+  {
+    "revision": "b9c20ed78b98bd685908",
+    "url": "/COM623_PROJECT/static/js/2.72e524e6.chunk.js"
+  },
+  {
+    "revision": "d6a1448fc5114a2352a9",
+    "url": "/COM623_PROJECT/static/js/main.8976204c.chunk.js"
+  },
+  {
+    "revision": "811b76f3328af0ce8fb9",
+    "url": "/COM623_PROJECT/static/js/runtime~main.1de84509.js"
+  },
+  {
+    "revision": "6e8575eb8b40935f111067be6ad23d89",
+    "url": "/COM623_PROJECT/static/media/backgroundimg.6e8575eb.png"
+  },
+  {
+    "revision": "8f95220477b1fa4e68a664931861063b",
+    "url": "/COM623_PROJECT/static/media/chat.8f952204.png"
+  },
+  {
+    "revision": "dfd418e35bca021223c4e3c96ec77c67",
+    "url": "/COM623_PROJECT/static/media/deadline.dfd418e3.png"
+  }
+]);
